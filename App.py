44187e7e06d580cc096ada64563e9046b2844591
isabella <isabella.bolivar@correo.unimet.edu.ve>
2024-08-07 16:30:13 -0400

    arregle crear misiones

--- App.py
+++ App.py
@@ -6,6 +6,7 @@ from ApiPeople import cargar_api
 from ApiFilms import cargar_api
 from Film import Film
 import csv
+
 file_path="C:\Users\Gaby_\Downloads\starwars.zip\csv"
 with open (file_path,"r") as file:
     csv_reader=csv.reader(file)
@@ -45,43 +46,53 @@ class App:
 
 
     def crear_mision(self):
-        print("Creemos una nueva misión")
-        nombre_mision=input("Ingrese el nombre de la mision: ")
-        while True:
-            planeta_destino = input("Ingrese el planeta destino: ")
-        #    if planeta_destino not in planets_obj
-         #       print("Planeta inválido")
-         #   else:
-            break
+        i=0
+        while i<5:
+            print("Creemos una nueva misión")
+
+            while True:
+                nombre_mision=input("Ingrese el nombre de la mision: ")
+                planeta_destino = input("Ingrese el planeta destino: ")
+                while  planeta_destino not in self.planets_obj:
+                    print("Planeta inválido. Intente nuevamente")
+                    planeta_destino = input("Ingrese el planeta destino: ")
+                
+                nave_utilizar = input("Ingrese la nave a utilizar: ")  
+                while nave_utilizar not in self.spaceships_obj:
+                    print("Nave inválida. Intente nuevamente.")
+                    nave_utilizar = input("Ingrese la nave a utilizar: ")
+
+                armas_utilizar = []
+                n = 0
+
+                while n < 7:
+                    arma_seleccionada = input("Ingrese una arma o 0 para finalizar: ")
 
 
-        armas_utilizar = []
-        n = 0
+                    if arma_seleccionada==0:
+                        break
+                    elif arma_seleccionada in self.weapons_obj:
+                        armas_utilizar.append(arma_seleccionada)
+                        n += 1
+                    else:
+                        print("Arma inválida. Intente nuevamente.")
 
-        while n < 7:
-            arma_seleccionada = input("Ingrese una arma o 0 para finalizar: ")
+                integrantes_mision=[]
+                m=0
 
-
-            if arma_seleccionada==0:
-                break
-            elif arma_seleccionada in self.weapons_obj:
-                armas_utilizar.append(arma_seleccionada)
-                n += 1
-            else:
-                print("Arma inválida. Intente nuevamente.")
-
-        integrantes_mision=[]
-        m=0
-
-        while m<7:
-            integrante_seleccionado= input("Ingrese un integrante de la misión o 0 para finalizar: ")
-            if integrante_seleccionado==0:
-                break
-            elif integrante_seleccionado in self.characters_obj:
-                integrantes_mision.append(integrante_seleccionado)
-                m+=1
-            else:
-                print("Integrante inválido. Intente nuevamente")
+                while m<7:
+                    integrante_seleccionado= input("Ingrese un integrante de la misión o 0 para finalizar: ")
+                    if integrante_seleccionado==0:
+                        break
+                    elif integrante_seleccionado in self.characters_obj:
+                        integrantes_mision.append(integrante_seleccionado)
+                        m+=1
+                    else:
+                        print("Integrante inválido. Intente nuevamente")
+                        
+        print('Mision creada con exito')
+        
+        i+=1
 
     
     def menu():

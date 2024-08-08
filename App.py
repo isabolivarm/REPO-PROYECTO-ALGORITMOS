320498ce851be00043b95360021ed130d56de104
isabella <isabella.bolivar@correo.unimet.edu.ve>
2024-08-08 16:12:14 -0400

    grafico personajes por planeta

--- App.py
+++ App.py
@@ -159,24 +159,24 @@ class App:
         dict_characters = {row[1]: row[10] for row in data[1:]}  
         return dict_characters
 
-def graficos_personajes_planetas(dict_characters):
-    planetas = {}
-    for character, planeta in dict_characters.items():
-        if planeta in planetas: 
-            planetas[planeta] += 1
-        else: 
-            planetas[planeta] = 1
+    def graficos_personajes_planetas(dict_characters):
+        planetas = {}
+        for character, planeta in dict_characters.items():
+            if planeta in planetas: 
+                planetas[planeta] += 1
+            else: 
+                planetas[planeta] = 1
 
-    plt.bar(planetas.keys(), planetas.values())
-    plt.xlabel("Planeta")
-    plt.ylabel("Cantidad de personajes")
-    plt.title("Cantidad de personajes por planeta")
-    plt.xticks(rotation=90)  
-    plt.show()
+        plt.bar(planetas.keys(), planetas.values())
+        plt.xlabel("Planeta")
+        plt.ylabel("Cantidad de personajes")
+        plt.title("Cantidad de personajes por planeta")
+        plt.xticks(rotation=90)  
+        plt.show()
 
-dict_characters = abrir_characters()
-graficos_personajes_planetas(dict_characters)
-    
+    dict_characters = abrir_characters()
+    graficos_personajes_planetas(dict_characters)
+        
 
     
 

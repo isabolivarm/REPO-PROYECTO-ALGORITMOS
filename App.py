8398299501db59f19536bb38fcd62eedb4e2ed3d
isabella <isabella.bolivar@correo.unimet.edu.ve>
2024-08-08 15:37:07 -0400

    abrir los csv de los characters

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ from ApiFilms import cargar_api
 from Film import Film
 from Weapon import Weapon
 from Starship import Starship
-
+import csv
 
 class App:
     film_obj=[]
@@ -122,6 +122,19 @@ class App:
         
         i+=1
 
+    def transformar_weapons(self): #TENGO QUE REVISAR ESTO
+            for weapon_name in self.lista_csv_weapons:
+                self.weapons_obj.append(Weapon(weapon_name))
+
+
+
+    def abrir_characters(self):
+        with open('characters.csv', 'r') as csv_characters:
+            reader = csv.reader(csv_characters)
+            data = list(reader) 
+        dict_characters = {row[1]: row[11] for row in data[1:]}  
+        return dict_characters
+ 
 
 
     
